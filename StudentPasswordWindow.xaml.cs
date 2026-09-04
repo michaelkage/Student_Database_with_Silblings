d7f45f60d67ad598973c48ccfe75904b5f43ee17
Michael Kage <michaelkage9@gmail.com>
2026-09-04 18:08:42 +0100

    Implement original StudentPasswordChange flow

---
+++ StudentPasswordWindow.xaml.cs
@@ -0,0 +1,31 @@
+using System.Windows;
+
+namespace StudentManagementApp;
+
+public partial class StudentPasswordWindow : Window
+{
+    private readonly Student student;
+
+    public StudentPasswordWindow(Student student)
+    {
+        InitializeComponent();
+        this.student = student;
+    }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+        string password = CurrentPasswordBox.Password;
+        if (password == student.StudentPassword)
+        {
+            string newPass = NewPasswordBox.Password;
+            student.StudentPassword = newPass;
+            MainWindow.SaveMemory();
+            MessageBox.Show("Success!!!");
+            Close();
+        }
+        else
+        {
+            MessageBox.Show("Fail :(");
+        }
+    }
+}
